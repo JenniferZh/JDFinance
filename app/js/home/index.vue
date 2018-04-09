4dd3fd42aba998d0e1bbbd05455109fe90c3c0d5
JenniferZh <jenniferzhang0606@gmail.com>
2018-04-09 12:35:52 +0800

    'first'

---
+++ app/js/home/index.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+    <div>
+        <Heador/>
+        <Slider/>
+        <Novice/>
+        <Borrow></Borrow>
+    </div>
+</template>
+
+<script>
+import Heador from '../public/header.vue'
+import Slider from '../home/hslider.vue'
+import Novice from '../home/novice.vue'
+import Borrow from '../home/borrow.vue'
+export default {
+  components: {
+    Heador,
+    Slider,
+    Novice,
+    Borrow,
+  }
+}
+</script>
+
+<style lang="scss" module>
+
+</style>
